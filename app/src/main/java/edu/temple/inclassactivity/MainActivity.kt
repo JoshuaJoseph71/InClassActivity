c3990a8ac1502defe9e0578b344b80bc8e826bba
Joshua Joseph <tuf09815@temple.edu>
2023-03-02 11:40:30 -0500

    submission

--- app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/inclassactivity/MainActivity.kt
@@ -2,9 +2,13 @@ package edu.temple.inclassactivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Adapter
+import android.widget.AdapterView
 import android.widget.Spinner
 import android.widget.SpinnerAdapter
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +22,21 @@ class MainActivity : AppCompatActivity() {
 
         // Fetch images into Array<Int> called imageArray
         val typedArray = resources.obtainTypedArray(R.array.image_ids)
-        val imageArray = Array(typedArray.length()) {typedArray.getResourceId(it, 0)}
+        val imageArray = Array(typedArray.length()) { typedArray.getResourceId(it, 0) }
         typedArray.recycle()
 
         // Display images specified in imageArray in Spinner and RecyclerView
+
+        findViewById<Spinner>(R.id.spinner)?.apply {
+            adapter = CustomSpinnerAdapter(this@MainActivity, imageArray)
+        }
+
+        findViewById<RecyclerView>(R.id.recyclerView)?.apply {
+            adapter = CustomRecyclerAdapter(imageArray)
+            layoutManager = LinearLayoutManager(this@MainActivity)
+
+        }
+
 
     }
 }
